ce19ec72aa6a4c8c9caccc67528783aef2b64169
n.tukmachev <n.tukmachev@tinkoff.ru>
2020-08-13 22:47:30 +0300

    create new project

---
+++ superPots/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  superPots
+//
+//  Created by n.tukmachev on 13.08.2020.
+//  Copyright © 2020 tinkoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
